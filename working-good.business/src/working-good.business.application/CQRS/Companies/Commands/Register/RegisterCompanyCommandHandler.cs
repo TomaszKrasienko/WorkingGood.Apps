893cf0ab2cc06ad8699a063c45fb46f05f2405a7
Tomasz Krasieńko <tomaszkrasienko@Tomaszs-MacBook-Pro.local>
2023-12-29 10:44:39 +0100

    Added test for registration owner company

--- working-good.business/src/working-good.business.application/CQRS/Companies/Commands/Register/RegisterCompanyCommandHandler.cs
+++ working-good.business/src/working-good.business.application/CQRS/Companies/Commands/Register/RegisterCompanyCommandHandler.cs
@@ -1,9 +1,20 @@
 using working_good.business.application.CQRS.Abstractions;
+using working_good.business.core.Abstractions.Repositories;
+using working_good.business.core.DomainServices.Abstractions;
 
 namespace working_good.business.application.CQRS.Companies.Commands.Register;
 
 internal sealed class RegisterCompanyCommandHandler : ICommandHandler<RegisterCompanyCommand>
 {
+    private readonly ICompanyRegistrationService _companyRegistrationService;
+    private readonly ICompanyRepository _companyRepository;
+
+    public RegisterCompanyCommandHandler(ICompanyRegistrationService companyRegistrationService, ICompanyRepository companyRepository)
+    {
+        _companyRegistrationService = companyRegistrationService;
+        _companyRepository = companyRepository;
+    }
+
     public Task HandleAsync(RegisterCompanyCommand command, CancellationToken token)
     {
         throw new NotImplementedException();
--- working-good.business/tests/working-good.business.unit-tests/working-good.business.application.tests/CQRS/Companies/Commands/RegisterCompanyCommandHandlerTests.cs
+++ working-good.business/tests/working-good.business.unit-tests/working-good.business.application.tests/CQRS/Companies/Commands/RegisterCompanyCommandHandlerTests.cs
@@ -1,6 +1,47 @@
+using Moq;
+using working_good.business.application.CQRS.Companies.Commands.Register;
+using working_good.business.core.Abstractions.Repositories;
+using working_good.business.core.DomainServices;
+using working_good.business.core.DomainServices.Abstractions;
+using working_good.business.core.Models.Company;
+using Xunit;
+
 namespace working_good.business.application.tests.CQRS.Companies.Commands;
 
-public class RegisterCompanyCommandHandlerTests
+public sealed class RegisterCompanyCommandHandlerTests
 {
+    [Fact]
+    public async Task Handle_ForRegisterCompanyCommandWithOwnerField_ShouldAddCompanyByRepository()
+    {
+        //arrange
+        _companyRepositoryMock
+            .Setup(f => f.GetAllAsync());
+        var command = new RegisterCompanyCommand(Guid.NewGuid(), "testOwnerCompanyName", true,
+            "test.pl");
+
+        //act
+        await _handler.HandleAsync(command, default);
+        
+        //assert
+        _companyRepositoryMock
+            .Verify(f => f.AddAsync(It.Is<Company>(arg
+                => arg.Id == command.Id
+                && arg.Name == command.Name
+                && arg.IsOwner == command.IsOwner
+                && arg.EmailDomain == command.EmailDomain)));
+    }
     
+    #region arrange
+
+    private readonly Mock<ICompanyRepository> _companyRepositoryMock;
+    private readonly ICompanyRegistrationService _companyRegistrationService;
+    private readonly RegisterCompanyCommandHandler _handler;
+    public RegisterCompanyCommandHandlerTests()
+    {
+        _companyRepositoryMock = new Mock<ICompanyRepository>();
+        _companyRegistrationService = new CompanyRegistrationService();
+        _handler = new RegisterCompanyCommandHandler(_companyRegistrationService, _companyRepositoryMock.Object);
+    }
+
+    #endregion
 }

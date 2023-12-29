c0fc8b6c549db9691d570accc732bf7a83fd3569
Tomasz Krasieńko <tomaszkrasienko@Tomaszs-MacBook-Pro.local>
2023-12-29 10:49:37 +0100

    Added test for registration company

--- working-good.business/tests/working-good.business.unit-tests/working-good.business.application.tests/CQRS/Companies/Commands/RegisterCompanyCommandHandlerTests.cs
+++ working-good.business/tests/working-good.business.unit-tests/working-good.business.application.tests/CQRS/Companies/Commands/RegisterCompanyCommandHandlerTests.cs
@@ -31,16 +31,50 @@ public sealed class RegisterCompanyCommandHandlerTests
                 && arg.EmailDomain == command.EmailDomain)));
     }
     
-    #region arrange
+    [Fact]
+    public async Task Handle_ForRegisterCompanyCommandWithExistedCompanies_ShouldAddCompanyByRepository()
+    {
+        //arrange
+        List<Company> companies = new List<Company>()
+        {
+            _company,
+            _ownerCompany
+        };
+        _companyRepositoryMock
+            .Setup(f => f.GetAllAsync())
+            .ReturnsAsync(companies);
+        var command = new RegisterCompanyCommand(Guid.NewGuid(), "newCompany", false,
+            "test.pl", TimeSpan.FromDays(1));
 
+        //act
+        await _handler.HandleAsync(command, default);
+        
+        //assert
+        _companyRepositoryMock
+            .Verify(f => f.AddAsync(It.Is<Company>(arg
+                => arg.Id == command.Id
+                   && arg.Name == command.Name
+                   && arg.IsOwner == command.IsOwner
+                   && arg.EmailDomain == command.EmailDomain
+                   && arg.SlaTimeSpan.Value == command.SlaTimeSpan)));
+    }
+    
+    #region arrange
     private readonly Mock<ICompanyRepository> _companyRepositoryMock;
     private readonly ICompanyRegistrationService _companyRegistrationService;
     private readonly RegisterCompanyCommandHandler _handler;
+    
+    private readonly Company _company;
+    private readonly Company _ownerCompany;
     public RegisterCompanyCommandHandlerTests()
     {
         _companyRepositoryMock = new Mock<ICompanyRepository>();
         _companyRegistrationService = new CompanyRegistrationService();
         _handler = new RegisterCompanyCommandHandler(_companyRegistrationService, _companyRepositoryMock.Object);
+        _company = Company.CreateCompany(Guid.NewGuid(), "testCompanyName", 
+            new TimeSpan(10,10,10), "test.pl");
+        _ownerCompany = Company.CreateOwnerCompany(Guid.NewGuid(), "testOwnerCompanyName",
+            "owner.pl");
     }
 
     #endregion
